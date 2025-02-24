59ed8506ed208cff7713ce15a25175ec2984ec26
Gabriel Costa Matos <gcosta-m@c3r12p4.42sp.org.br>
2025-02-24 11:46:33 -0300

    check_quotes ok

--- tests/test.c
+++ tests/test.c
@@ -1,21 +1,52 @@
 #include "minunit.h"
 #include "../includes/minishell.h"
 
-MU_TEST(check_quotes_test)
+MU_TEST(check_quotes_basic_test)
 {
-	printf("--------------------\n");
-	printf("TEST: check_quotes_test\n");
-	printf("--------------------\n");
+	printf("------------------------------\n");
+	printf("TESTE: Casos Básicos de Aspas\n");
+	printf("------------------------------\n");
 
-    mu_assert_int_eq(check_quotes('\"', 0), 1);
-    mu_assert_int_eq(check_quotes('\"', 1), 0);
-    mu_assert_int_eq(check_quotes('\'', 0), 2);
-    mu_assert_int_eq(check_quotes('\'', 2), 0);
+	mu_assert_int_eq(check_quotes('\"', 0), 1);  // Abre aspas duplas
+	mu_assert_int_eq(check_quotes('\"', 1), 0);  // Fecha aspas duplas
+	mu_assert_int_eq(check_quotes('\'', 0), 2);  // Abre aspas simples
+	mu_assert_int_eq(check_quotes('\'', 2), 0);  // Fecha aspas simples
 }
+
+MU_TEST(check_quotes_edge_cases)
+{
+	printf("--------------------------------\n");
+	printf("TESTE: Casos Limite e Erros\n");
+	printf("--------------------------------\n");
+
+	mu_assert_int_eq(check_quotes('\"', 2), 2);  // Aspas duplas abertas dentro de aspas simples
+	mu_assert_int_eq(check_quotes('\'', 1), 1);  // Aspas simples abertas dentro de aspas duplas
+	mu_assert_int_eq(check_quotes('a', 0), 0);   // Caractere comum sem aspas abertas
+	mu_assert_int_eq(check_quotes('{', 1), 1);   // Caracteres aleatórios dentro de aspas duplas
+	mu_assert_int_eq(check_quotes('}', 2), 2);   // Caracteres aleatórios dentro de aspas simples
+}
+
+MU_TEST(check_quotes_complex_cases)
+{
+	printf("---------------------------------\n");
+	printf("TESTE: Casos Complexos e Aninhados\n");
+	printf("---------------------------------\n");
+
+	mu_assert_int_eq(check_quotes('\"', 0), 1);  // Abre aspas duplas
+	mu_assert_int_eq(check_quotes('\'', 1), 1);  // Aspas simples dentro de aspas duplas (mantém aberto)
+	mu_assert_int_eq(check_quotes('\"', 1), 0);  // Fecha aspas duplas
+
+	mu_assert_int_eq(check_quotes('\'', 0), 2);  // Abre aspas simples
+	mu_assert_int_eq(check_quotes('\"', 2), 2);  // Aspas duplas dentro de aspas simples (ignora)
+	mu_assert_int_eq(check_quotes('\'', 2), 0);  // Fecha aspas simples
+}
+
 
 MU_TEST_SUITE(test_suite)
 {
-    MU_RUN_TEST(check_quotes_test);
+    MU_RUN_TEST(check_quotes_basic_test);
+	MU_RUN_TEST(check_quotes_edge_cases);
+	MU_RUN_TEST(check_quotes_complex_cases);
 }
 
 int main(void)
