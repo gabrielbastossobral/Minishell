bd54e9d4c26db5efd0b1d866e2ad0fc50580259e
Gabriel Bastos Sobral <gabastos@c1r3p1.42sp.org.br>
2025-03-11 13:30:00 -0300

    fix problem cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -3,122 +3,113 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosta-m <gcosta-m@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabastos <gabastos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 09:25:13 by gabastos          #+#    #+#             */
-/*   Updated: 2025/03/11 10:46:58 by gcosta-m         ###   ########.fr       */
+/*   Updated: 2025/03/11 11:42:13 by gabastos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void set_dir(t_data *ms, char *dir, char ***envp)
+static void	set_dir(t_data *ms, char *dir, char ***envp)
 {
-    char *temp;
-    char **var;
-    
-    // Early return if dir is NULL
-    if (!dir) {
-        ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-        ms->error = 1;
-        return;
-    }
-    
-    // Allocate buffer for getcwd
-    temp = malloc(BUFFER_SIZE);
-    if (!temp) {
-        ms->error = 1;
-        return;
-    }
-    
-    var = ft_calloc(3, sizeof(char *));
-    if (!var) {
-        free(temp);
-        ms->error = 1;
-        return;
-    }
-    
-    if (getcwd(temp, BUFFER_SIZE) == NULL) {
-        free(temp);
-        free(var);
-        ms->error = 1;
-        return;
-    }
-    
-    var[0] = ft_strdup("export");
-    if (!var[0]) {
-        free(temp);
-        free(var);
-        ms->error = 1;
-        return;
-    }
-    
-    if (chdir(dir)) {
-        perror("minishell: cd");
-        ms->error = 1;
-    } else {
-        var[1] = ft_strjoin("OLDPWD=", temp);
-        ft_export(ms, var, envp);
-        free(var[1]);
-        
-        if (getcwd(temp, BUFFER_SIZE) == NULL) {
-            ms->error = 1;
-        } else {
-            var[1] = ft_strjoin("PWD=", temp);
-            ft_export(ms, var, envp);
-            free(var[1]);
-            ms->error = 0;
-        }
-    }
-    
-    free(temp);
-    free(var[0]);
-    free(var);
+	char	*oldpwd;
+	char	*pwd;
+	char	**var;
+
+	oldpwd = getcwd(NULL, 0);
+	if (!oldpwd)
+	{
+		perror(PROMPT_MSG ": cd");
+		ms->error = 1;
+		return ;
+	}
+	if (chdir(dir) != 0)
+	{
+		perror(PROMPT_MSG ": cd");
+		ms->error = 1;
+		free(oldpwd);
+		return ;
+	}
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		perror(PROMPT_MSG ": cd");
+		ms->error = 1;
+		free(oldpwd);
+		return ;
+	}
+	var = ft_calloc(3, sizeof(char *));
+	if (!var)
+	{
+		free(oldpwd);
+		free(pwd);
+		ms->error = 1;
+		return ;
+	}
+	var[0] = ft_strdup("export");
+	var[1] = ft_strjoin("OLDPWD=", oldpwd);
+	ft_export(ms, var, envp);
+	free(var[1]);
+	var[1] = ft_strjoin("PWD=", pwd);
+	ft_export(ms, var, envp);
+	free(var[1]);
+	free(var[0]);
+	free(var);
+	free(oldpwd);
+	free(pwd);
+	ms->error = 0;
 }
 
-static char *get_home(char **envp)
+static char	*get_home(char **envp)
 {
-    int i;
-    char *home = NULL;
-    
-    i = 0;
-    if (!envp)
-        return NULL;
-        
-    while (envp[i]) {
-        if (!ft_strncmp(envp[i], "HOME=", 5)) {
-            home = ft_strdup(envp[i] + 5);
-            break;
-        }
-        i++;
-    }
-    
-    return home;
+	int		i;
+	char	*home;
+
+	home = NULL;
+	i = 0;
+	if (!envp)
+		return (NULL);
+	while (envp[i])
+	{
+		if (!ft_strncmp(envp[i], "HOME=", 5))
+		{
+			home = ft_strdup(envp[i] + 5);
+			break ;
+		}
+		i++;
+	}
+	return (home);
 }
 
-void ft_cd(t_data *ms, char **cmd, char ***envp)
+void	ft_cd(t_data *ms, char **cmd, char ***envp)
 {
-    char *home_dir = NULL;
-    
-    if (!cmd) {
-        ms->error = 1;
-        return;
-    }
-    
-    if (cmd[1] && !cmd[2])
-        set_dir(ms, cmd[1], envp);
-    else if (!cmd[1]) {
-        home_dir = get_home(*envp);
-        if (!home_dir) {
-            ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
-            ms->error = 1;
-            return;
-        }
-        set_dir(ms, home_dir, envp);
-        free(home_dir);
-    }
-    else {
-        ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
-        ms->error = 1;
-    }
+	char	*home_dir;
+
+	home_dir = NULL;
+	if (!cmd)
+	{
+		ms->error = 1;
+		return ;
+	}
+	if (cmd[1] && !cmd[2])
+		set_dir(ms, cmd[1], envp);
+	else if (!cmd[1])
+	{
+		home_dir = get_home(*envp);
+		if (!home_dir)
+		{
+			ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
+			ms->error = 1;
+			return ;
+		}
+		set_dir(ms, home_dir, envp);
+		free(home_dir);
+	}
+	else
+	{
+		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
+		ms->error = 1;
+	}
 }
